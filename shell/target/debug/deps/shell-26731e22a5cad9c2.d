55eedb3b73ed69d001f5c60b4fbbde250d1649e1
chemi123 <chemiec@gmail.com>
2023-01-14 09:51:53 +0900

    add shell

---
+++ shell/target/debug/deps/shell-26731e22a5cad9c2.d
@@ -0,0 +1,5 @@
+/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-26731e22a5cad9c2.rmeta: src/main.rs
+
+/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-26731e22a5cad9c2.d: src/main.rs
+
+src/main.rs:
---
+++ shell/target/debug/deps/shell-954a9583f7b04eae.d
@@ -0,0 +1,5 @@
+/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-954a9583f7b04eae.rmeta: src/main.rs
+
+/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-954a9583f7b04eae.d: src/main.rs
+
+src/main.rs:
