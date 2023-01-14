0a7ed9a575f0d1480624326aa499fae8e70da5f9
chemi123 <chemiec@gmail.com>
2023-01-14 18:42:21 +0900

    simple shell without pipe function

--- shell/src/main.rs
+++ shell/src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    env,
+    io::{stdin, stdout, Write},
+    path::Path,
+    process::Command,
+};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let current_dir = env::current_dir().unwrap();
+        print!("{} $ ", current_dir.display());
+        stdout().flush().unwrap();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match command {
+            "cd" => {
+                let new_dir = args.peekable().peek().map_or("/", |x| *x);
+                let root = Path::new(new_dir);
+                if let Err(e) = env::set_current_dir(&root) {
+                    println!("{}", e);
+                }
+            }
+            "exit" => return,
+            _ => {
+                let child = Command::new(command).args(args).spawn();
+
+                match child {
+                    Ok(mut c) => {
+                        c.wait().unwrap();
+                    }
+                    Err(e) => eprintln!("{}", e),
+                }
+            }
+        }
+    }
 }
