aee64d4f1ab3cfbdc827a267b6faa2addbf0db05
chemi123 <chemiec@gmail.com>
2023-01-15 00:30:19 +0900

    delete unnecessary files

--- shell/target/debug/deps/shell-26731e22a5cad9c2.d
+++
@@ -1,5 +0,0 @@
-/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-26731e22a5cad9c2.rmeta: src/main.rs
-
-/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-26731e22a5cad9c2.d: src/main.rs
-
-src/main.rs:
--- shell/target/debug/deps/shell-954a9583f7b04eae.d
+++
@@ -1,5 +0,0 @@
-/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-954a9583f7b04eae.rmeta: src/main.rs
-
-/Users/chemi/workspace/rust-practice-project/shell/target/debug/deps/shell-954a9583f7b04eae.d: src/main.rs
-
-src/main.rs:
