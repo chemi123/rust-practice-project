97b45eef7dd5777be76363b508dc65ef83802de9
chemi123 <chemiec@gmail.com>
2023-01-27 10:27:53 +0900

    fix bin

--- risp/examples/sample.rs
+++ risp/examples/sample.rs
@@ -1,20 +1,17 @@
-use risp::{
-    core::{
-        engine::{parse, tokenize},
-        engine2::{parse2, tokenize2},
-    },
-    entity::risp_exp::print_risp_exp,
+use risp::core::{
+    engine::{parse, tokenize},
+    engine2::{parse2, tokenize2},
 };
 
 fn main() {
-    let tokens = tokenize("( + 1 2 3) 1 2 3");
+    let tokens = tokenize("( + 1 (- 2 3)) 1 2 3");
     let (risp_exp, str_vec) = parse(&tokens).unwrap();
-    print_risp_exp(&risp_exp);
+    println!("{}", risp_exp.to_string());
     println!("{:?}", str_vec);
     println!();
 
-    let tokens = tokenize2("( + 1 2 3) 1 2 3");
-    let (risp_exp, str_vec) = parse2(tokens).unwrap();
-    print_risp_exp(&risp_exp);
+    let mut tokens = tokenize2("( + 1 (- 2 3)) 1 2 3");
+    let (risp_exp, str_vec) = parse2(&mut tokens).unwrap();
+    println!("{}", risp_exp.to_string());
     println!("{:?}", str_vec);
 }
--- risp/src/bin/main.rs
+++ risp/src/bin/main.rs
@@ -1,13 +1,10 @@
 use risp::{
     core,
-    entity::{
-        risp_err::RispErr,
-        risp_exp::{print_risp_exp, RispExp},
-    },
+    entity::{risp_err::RispErr, risp_exp::RispExp},
 };
 fn main() {
     let exp = RispExp::Symbol(String::from("str"));
     let err = RispErr::Reason(String::from("reason"));
-    println!("{:?} {:?}", print_risp_exp(&exp), err,);
+    println!("{} {:?}", exp, err,);
     println!("{:?}", core::engine::tokenize(&String::from("(+ 10 1)")));
 }
