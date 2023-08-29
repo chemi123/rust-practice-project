56027f28d09315d95f8e52ca4e26c7126d2597d5
chemi123 <chemi123@example.com>
2023-08-29 14:49:42 +0900

    refactor

---
+++ lisp/lisp-rs/src/lisp_expr.rs
@@ -0,0 +1,49 @@
+use std::fmt::Display;
+
+pub enum LispExpr {
+    Void,
+    Integer(i64),
+    Symbol(String),
+    Lambda(Vec<String>, Vec<LispExpr>),
+    List(Vec<LispExpr>),
+}
+
+impl Display for LispExpr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            LispExpr::Void => Ok(()),
+            LispExpr::Integer(n) => write!(f, "{}", n),
+            LispExpr::Symbol(s) => write!(f, "{}", s),
+            LispExpr::Lambda(params, lisp_exprs) => {
+                let lisp_exprs_string = lisp_exprs.iter()
+                    .map(|lisp_expr| lisp_expr.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ");
+                write!(f, "(lambda ({}) ({}))", params.join(" "), lisp_exprs_string)
+            },
+            LispExpr::List(lisp_exprs) =>  {
+                let lisp_exprs_string = lisp_exprs.iter()
+                    .map(|lisp_expr| lisp_expr.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" ");
+                write!(f, "({})", lisp_exprs_string)
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_display() {
+        let lisp_exprs = LispExpr::Lambda(vec!["x".to_string(), "y".to_string()], vec![
+            LispExpr::Symbol("+".to_string()),
+            LispExpr::Integer(1),
+            LispExpr::Integer(1),
+            LispExpr::Void,
+        ]);
+        println!("{}", lisp_exprs);
+    }
+}
--- lisp/lisp-rs/src/main.rs
+++ lisp/lisp-rs/src/main.rs
@@ -1,51 +1,39 @@
-use std::{rc::Rc, cell::RefCell};
+mod lexer;
+mod lisp_expr;
+mod parser;
+mod token;
 
 use anyhow::Result;
-use env::Env;
-use linefeed::{Interface, ReadResult};
-
-use crate::parser::Object;
-
-mod env;
-mod eval;
-mod lexer;
-mod parser;
-
-const PROMPT: &str = "lisp-rs> ";
+use rustyline::{Editor, history::FileHistory, error::ReadlineError};
 
 fn main() -> Result<()> {
-    let reader = Interface::new(PROMPT).unwrap();
-    let mut env = Rc::new(RefCell::new(Env::new()));
+    let mut rl = Editor::<(), FileHistory>::new()?;
+    let _ = rl.load_history(".mal-history");
 
-    reader.set_prompt(format!("{}", PROMPT).as_ref())?;
-    while let ReadResult::Input(input) = reader.read_line().unwrap() {
-        if input.eq("exit") {
-            break;
-        }
-
-        let mut tokens = lexer::tokenize(input.as_str())?;
-        let object = parser::parse_tokens(&mut tokens)?;
-        let object = eval::eval_obj(&object, &mut env)?;
-
-        match object {
-            Object::Void => (),
-            Object::Integer(n) => println!("{}", n),
-            Object::Bool(b) => println!("{}", b),
-            Object::Symbol(s) => println!("{}", s),
-            Object::Lambda(params, body) => {
-                println!("Lambda(");
-                for param in params {
-                    println!("{} ", param);
-                }
-                println!(")");
-                for expr in body {
-                    println!(" {}", expr);
+    loop {
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(input) => {
+                if !input.trim().is_empty() {
+                    println!("{}", input);
+                    let _ = rl.add_history_entry(&input)?;
+                    let _ = rl.save_history(".lisp-rs-history")?;
                 }
             },
-            _ => println!("{}", object),
+            Err(ReadlineError::Interrupted) => {
+                println!("Interrupted.");
+                break;
+            },
+            Err(ReadlineError::Eof) => {
+                println!("Bye!");
+                break;
+            },
+            Err(err) => {
+                eprintln!("Error: {:?}", err);
+                break;
+            },
         }
     }
 
-    println!("good bye!");
     Ok(())
 }
